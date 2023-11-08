0e94ecdd80beef89697a79ddeb29cc157535f104
7th-Triforce <fer.delgadopedreira@gmail.com>
2023-11-08 19:11:03 +0100

    Initial commit. Creado el proyecto de C#

---
+++ Viajante de comercio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viajante_de_comercio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
