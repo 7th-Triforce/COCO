71be5dc8cd619d0413ff8a3638a222bf70215e0d
7th-Triforce <94680026+7th-Triforce@users.noreply.github.com>
2023-11-14 19:50:01 +0100

    Posible código funcional

--- Viajante de comercio/Program.cs
+++ Viajante de comercio/Program.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Viajante_de_comercio;
 
 class Program
 {
     static void Main()
     {
-        // Ejemplo de uso
-        int numeroDeVertices = 5;
-        GrafoPonderado grafo = new GrafoPonderado(numeroDeVertices);
-        int verticeInicial = 0;
 
+        Viajante viajero = new Viajante();
+        viajero.EmpezarPrograma();
+
+
+
+
+
+
+        
+        // Ejemplo de uso
+       const int numeroDeVertices = 4;
+        GrafoPonderado grafo = new GrafoPonderado(numeroDeVertices);
+        //int verticeInicial = 0;
+        /*
+        List<Arista>[] lista = grafo.getListaAdyacencia();
+        for (int i=0; i<numeroDeVertices; i++)
+        {
+            Console.WriteLine("Arista " + i);
+            foreach (Arista arista in lista[i])
+            {
+                Console.WriteLine(" - Destino: " + arista.Destino);
+                Console.WriteLine(" - Peso: " + arista.Peso);
+            }
+            Console.WriteLine();
+        }
+        */
+
+
+        /*
+        // A
+        Arista aristaAB = new Arista(1, 7);
+        Arista aristaAC = new Arista(2, 3);
+        Arista aristaAD = new Arista(3, 1);
+        // B
+        Arista aristaBD = new Arista(3, 1);
+        Arista aristaBA = new Arista(0, 7);
+        // C
+        Arista aristaCD = new Arista(3, 4);
+        Arista aristaCA = new Arista(0, 3);
+        //D
+        Arista aristaDA = new Arista(0, 1);
+        Arista aristaDB = new Arista(1, 1);
+        Arista aristaDC = new Arista(2, 4);
+
+        List<Arista>[] lista = new List<Arista>[numeroDeVertices] {
+            new List<Arista> {aristaAB, aristaAC, aristaAD},
+            new List<Arista> {aristaBA, aristaBD},
+            new List<Arista> {aristaCA, aristaCD},
+            new List<Arista> {aristaDA, aristaDB, aristaDC}};
+
+        grafo.setListaAdyacencia(lista);
         List<int> solucion = ProblemaViajante(grafo, verticeInicial);
 
         Console.WriteLine("Solución del Problema del Viajante:");
@@ -18,6 +66,7 @@ class Program
         {
             Console.Write($"{vertice} ");
         }
+        */
     }
 
     static List<int> ProblemaViajante(GrafoPonderado grafo, int vertice)
@@ -114,6 +163,15 @@ class Program
                     }
                 }
             }
+        }
+
+        public void setListaAdyacencia(List<Arista>[] lista)
+        {
+            listaDeAdyacencia = lista;
+        }
+        public List<Arista>[] getListaAdyacencia()
+        {
+            return listaDeAdyacencia;
         }
 
         public int AgregarAristaDirigida(int origen, int destino)
---
+++ Viajante de comercio/Viajante.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viajante_de_comercio
+{
+    internal class Viajante
+    {
+        public int V = 5; // Número de vértices en el grafo
+
+        // Función para encontrar la ruta más corta
+        private void TSP(int[,] grafo, int inicio)
+        {
+            int[] vertices = new int[V];
+            for (int i = 0; i < V; i++)
+            {
+                vertices[i] = i;
+            }
+
+            int[] ruta = new int[V + 1];
+            int pesoMinimo = int.MaxValue;
+
+            do
+            {
+                int pesoRuta = 0;
+                int k = inicio;
+                int i;
+                for (i = 0; i < V; i++)
+                {
+                    pesoRuta += grafo[k, vertices[i]];
+                    ruta[i] = vertices[i];
+                    k = vertices[i];
+                }
+
+                pesoRuta += grafo[k, inicio];
+
+                if (pesoRuta < pesoMinimo)
+                {
+                    pesoMinimo = pesoRuta;
+                    for (i = 0; i < V; i++)
+                    {
+                        Console.Write((char)(65 + ruta[i]) + " ");
+                    }
+                    Console.WriteLine((char)(65 + inicio));
+                }
+
+            } while (NextPermutation(vertices));
+        }
+
+        // Función para obtener la siguiente permutación
+        private bool NextPermutation(int[] array)
+        {
+            int i = array.Length - 1;
+            while (i > 0 && array[i - 1] >= array[i])
+            {
+                i--;
+            }
+
+            if (i <= 0)
+            {
+                return false;
+            }
+
+            int j = array.Length - 1;
+            while (array[j] <= array[i - 1])
+            {
+                j--;
+            }
+
+            int temp = array[i - 1];
+            array[i - 1] = array[j];
+            array[j] = temp;
+
+            j = array.Length - 1;
+            while (i < j)
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        // Función principal
+        public void EmpezarPrograma()
+        {
+            int[,] grafo = {
+                { 0, 5, 3, 7, 2 },
+                { 5, 0, 6, 8, 9 },
+                { 3, 6, 0, 4, 1 },
+                { 7, 8, 4, 0, 3 },
+                { 2, 9, 1, 3, 0 }
+            };
+
+            int inicio = 0; // Punto de partida
+
+            Console.WriteLine("Rutas posibles:");
+            TSP(grafo, inicio);
+        }
+    }
+}
