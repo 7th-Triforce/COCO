d8b553eb3875706a4f09b5c42cc6d93d6a810f4f
7th-Triforce <94680026+7th-Triforce@users.noreply.github.com>
2023-11-15 12:25:08 +0100

    Terminado el cálculo por líneas

--- Viajante de comercio/Viajante.cs
+++ Viajante de comercio/Viajante.cs
@@ -25,57 +25,56 @@ namespace Viajante_de_comercio
             {                                                            //
                 int pesoRuta = 0;                                        // 1
                 int k = inicio;                                          // 1
-                int i;                                                   //
-                for (i = 0; i < V; i++){                                 // 1, n+1, n
-                    pesoRuta += grafo[k, vertices[i]];                   // 1 + 1
-                    ruta[i] = vertices[i];                               // 1
-                    k = vertices[i];                                     // 1
+                for (int i = 0; i < V; i++){                             // 1, n+1, n
+                    pesoRuta += grafo[k, vertices[i]];                   // (1 + 1) * n
+                    ruta[i] = vertices[i];                               // n
+                    k = vertices[i];                                     // n
                 }
 
                 pesoRuta += grafo[k, inicio];                            // 1 + 1
 
-                if (pesoRuta < pesoMinimo){                              // 1
-                    pesoMinimo = pesoRuta;                               // 1 
-                    for (i = 0; i < V; i++){                             // 1, n+1, 1
-                        Console.Write((char)(65 + ruta[i]) + " ");       // 1 + 1
+                if (pesoRuta < pesoMinimo){                              // 1 | Encontró una ruta válida
+                    pesoMinimo = pesoRuta;                               // n 
+                    for (int i = 0; i < V; i++){                         // 1, n+1, n
+                        Console.Write((char)(65 + ruta[i]) + " ");       // (1 + 1) * n
                     }
                     Console.WriteLine((char)(65 + inicio));              // 1
                 }
-            } while (SiguientePermutacion(vertices));                    // 
+            } while (SiguientePermutacion(vertices));                    // n! es el máximo de permutaciones posibles -> n! * (11n + 10) 
         }
 
         // Función para obtener la siguiente permutación
         private bool SiguientePermutacion(int[] array)
         {
             int i = array.Length - 1;                                    // 1 + 1
-            while (i > 0 && array[i - 1] >= array[i])                    // n + n
-                i--;                                                     //
+            while (i > 0 && array[i - 1] >= array[i])                    // n + 2n
+                i--;                                                     // n
 
             if (i <= 0)                                                  // 1 
                 return false;
 
             int j = array.Length - 1;                                    // 1 + 1
-            while (array[j] <= array[i - 1])                             //
-                j--;                                                     //
+            while (array[j] <= array[i - 1])                             // n + n
+                j--;                                                     // n
 
-            int temp = array[i - 1];                                     //
-            array[i - 1] = array[j];                                     //
-            array[j] = temp;                                             //
+            int temp = array[i - 1];                                     // 1 + 1
+            array[i - 1] = array[j];                                     // 1 + 1
+            array[j] = temp;                                             // 1
 
-            j = array.Length - 1;                                        //
-            while (i < j){                                               //
-                temp = array[i];                                         //
-                array[i] = array[j];                                     //
-                array[j] = temp;                                         //
-                i++;                                                     //
-                j--;                                                     //
+            j = array.Length - 1;                                        // 1 + 1
+            while (i < j){                                               // n/2
+                temp = array[i];                                         // n/2
+                array[i] = array[j];                                     // n/2
+                array[j] = temp;                                         // n/2
+                i++;                                                     // n/2
+                j--;                                                     // n/2
             }
 
             return true;
         }
 
         // Función principal
-        public void EmpezarPrograma(int[,] grafo, int verticeInicio)
+        public void EmpezarPrograma(int[,] grafo, int verticeInicio) // O(n * n!) 
         {
             V = grafo.GetLength(0); // Filas del grafo, valdría con columnas
 
