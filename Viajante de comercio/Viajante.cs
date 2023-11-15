bc2ccde6702e947759829c59bb16e6400da19e19
7th-Triforce <94680026+7th-Triforce@users.noreply.github.com>
2023-11-15 11:07:07 +0100

    Calculo de la complejidad en proceso

--- Viajante de comercio/Viajante.cs
+++ Viajante de comercio/Viajante.cs
@@ -8,99 +8,79 @@ namespace Viajante_de_comercio
 {
     internal class Viajante
     {
-        public int V = 5; // Número de vértices en el grafo
+        public int V; // Número de vértices en el grafo
 
-        // Función para encontrar la ruta más corta
-        private void TSP(int[,] grafo, int inicio)
-        {
-            int[] vertices = new int[V];
-            for (int i = 0; i < V; i++)
-            {
-                vertices[i] = i;
+        // Función para encontrar la ruta más corta                                
+        private void TSP(int[,] grafo, int inicio)                       //
+        {                                                                //
+            int[] vertices = new int[V];                                 // 1
+            for (int i = 0; i < V; i++){                                 // 1, n+1, n
+                vertices[i] = i;                                         // n
             }
 
-            int[] ruta = new int[V + 1];
-            int pesoMinimo = int.MaxValue;
+            int[] ruta = new int[V + 1];                                 // 1
+            int pesoMinimo = int.MaxValue;                               // 1
 
-            do
-            {
-                int pesoRuta = 0;
-                int k = inicio;
-                int i;
-                for (i = 0; i < V; i++)
-                {
-                    pesoRuta += grafo[k, vertices[i]];
-                    ruta[i] = vertices[i];
-                    k = vertices[i];
+            do                                                           //
+            {                                                            //
+                int pesoRuta = 0;                                        // 1
+                int k = inicio;                                          // 1
+                int i;                                                   //
+                for (i = 0; i < V; i++){                                 // 1, n+1, n
+                    pesoRuta += grafo[k, vertices[i]];                   // 1 + 1
+                    ruta[i] = vertices[i];                               // 1
+                    k = vertices[i];                                     // 1
                 }
 
-                pesoRuta += grafo[k, inicio];
+                pesoRuta += grafo[k, inicio];                            // 1 + 1
 
-                if (pesoRuta < pesoMinimo)
-                {
-                    pesoMinimo = pesoRuta;
-                    for (i = 0; i < V; i++)
-                    {
-                        Console.Write((char)(65 + ruta[i]) + " ");
+                if (pesoRuta < pesoMinimo){                              // 1
+                    pesoMinimo = pesoRuta;                               // 1 
+                    for (i = 0; i < V; i++){                             // 1, n+1, 1
+                        Console.Write((char)(65 + ruta[i]) + " ");       // 1 + 1
                     }
-                    Console.WriteLine((char)(65 + inicio));
+                    Console.WriteLine((char)(65 + inicio));              // 1
                 }
-
-            } while (NextPermutation(vertices));
+            } while (SiguientePermutacion(vertices));                    // 
         }
 
         // Función para obtener la siguiente permutación
-        private bool NextPermutation(int[] array)
+        private bool SiguientePermutacion(int[] array)
         {
-            int i = array.Length - 1;
-            while (i > 0 && array[i - 1] >= array[i])
-            {
-                i--;
-            }
+            int i = array.Length - 1;                                    // 1 + 1
+            while (i > 0 && array[i - 1] >= array[i])                    // n + n
+                i--;                                                     //
 
-            if (i <= 0)
-            {
+            if (i <= 0)                                                  // 1 
                 return false;
-            }
 
-            int j = array.Length - 1;
-            while (array[j] <= array[i - 1])
-            {
-                j--;
-            }
+            int j = array.Length - 1;                                    // 1 + 1
+            while (array[j] <= array[i - 1])                             //
+                j--;                                                     //
 
-            int temp = array[i - 1];
-            array[i - 1] = array[j];
-            array[j] = temp;
+            int temp = array[i - 1];                                     //
+            array[i - 1] = array[j];                                     //
+            array[j] = temp;                                             //
 
-            j = array.Length - 1;
-            while (i < j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
+            j = array.Length - 1;                                        //
+            while (i < j){                                               //
+                temp = array[i];                                         //
+                array[i] = array[j];                                     //
+                array[j] = temp;                                         //
+                i++;                                                     //
+                j--;                                                     //
             }
 
             return true;
         }
 
         // Función principal
-        public void EmpezarPrograma()
+        public void EmpezarPrograma(int[,] grafo, int verticeInicio)
         {
-            int[,] grafo = {
-                { 0, 5, 3, 7, 2 },
-                { 5, 0, 6, 8, 9 },
-                { 3, 6, 0, 4, 1 },
-                { 7, 8, 4, 0, 3 },
-                { 2, 9, 1, 3, 0 }
-            };
-
-            int inicio = 0; // Punto de partida
+            V = grafo.GetLength(0); // Filas del grafo, valdría con columnas
 
             Console.WriteLine("Rutas posibles:");
-            TSP(grafo, inicio);
+            TSP(grafo, verticeInicio);
         }
     }
 }
